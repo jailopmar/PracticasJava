69dbcb216763e43a747a00857e5e8aca41980fd1
jailopmar <jaime96lopez@gmail.com>
2023-12-15 16:57:39 +0100

    Añadido el apartado C al ejercicio 2 de la practica individual 3

--- PracticaIndividual3/src/Ejercicios/Ejercicio2.java
+++ PracticaIndividual3/src/Ejercicios/Ejercicio2.java
@@ -1,5 +1,11 @@
 package Ejercicios;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import org.jgrapht.Graph;
 import org.jgrapht.GraphPath;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
@@ -9,6 +15,7 @@ import Tipos.Atraccion;
 import Tipos.Vecindad;
 import us.lsi.colors.GraphColors;
 import us.lsi.colors.GraphColors.Color;
+import us.lsi.common.List2;
 
 public class Ejercicio2 {
 
@@ -27,19 +34,89 @@ public class Ejercicio2 {
 
 		return shortestPath;
 	}
-	
-	public static GraphPath<Atraccion, Vecindad> ejercicio2B(Graph<Atraccion, Vecindad> g, String fichero){
-		
-		HeldKarpTSP<Atraccion, Vecindad> alg = new HeldKarpTSP<Atraccion, Vecindad>();		
+
+	public static GraphPath<Atraccion, Vecindad> ejercicio2B(Graph<Atraccion, Vecindad> g, String fichero) {
+
+		HeldKarpTSP<Atraccion, Vecindad> alg = new HeldKarpTSP<Atraccion, Vecindad>();
 		GraphPath<Atraccion, Vecindad> res = alg.getTour(g);
-		
+
 		GraphColors.toDot(g, carpetaResultados + fichero + "ApartadoB.gv", v -> v.nombre(),
 				e -> "(distancia: " + e.distanciaKm() + ", tiempo: " + e.tiempo() + ")",
-				v -> GraphColors.color(res.getVertexList().get(0).equals(v) ? Color.magenta:Color.blue),
+				v -> GraphColors.color(res.getVertexList().get(0).equals(v) ? Color.magenta : Color.blue),
 				e -> GraphColors.colorIf(Color.blue, res.getEdgeList().contains(e)));
 
-		
 		return res;
-		
+
+	}
+
+	// Apartado C
+
+	public static List<Atraccion> ejercicio2C(Graph<Atraccion, Vecindad> g, Integer numeroHoras) {
+
+		// Declaración de un comparador para ordenar por popularidad
+		Comparator<Atraccion> comparacionPopularidad = (atraccion1, aatraccion2) -> atraccion1.popularidad()
+				.compareTo(aatraccion2.popularidad());
+
+		// Obtención de la atracción más popular usando el comparador
+		Atraccion masPopular = g.vertexSet().stream().max(comparacionPopularidad).get();
+
+		// Cálculo de las horas restantes restando la duración y el tiempo de espera de
+		// la atracción más popular
+		Double horasRestantes = numeroHoras - (masPopular.duracion() + masPopular.tiempo()) / 60.;
+
+		return ejercicio2C(g, horasRestantes, masPopular, List2.empty());
+	}
+
+	private static List<Atraccion> ejercicio2C(Graph<Atraccion, Vecindad> g, Double horasRestantes,
+			Atraccion masPopular, List<Atraccion> res) {
+
+		//Si no hay horas restantes o no hay atracciones para visitar
+		if (horasRestantes <= 0 || masPopular == null) {
+			return res;
+
+			
+		} else {
+			//Agregar atracción actual a la lista
+			res.add(masPopular);
+
+			// Encuentra el vecino más popular y actualiza las horas restantes
+			Atraccion vecino = vecinoMasPopular(masPopular, g, res);
+			Vecindad path = g.getEdge(masPopular, vecino);
+			horasRestantes -= tiempoPorAtraccion(vecino, path);
+
+			return ejercicio2C(g, horasRestantes, vecino, res);
+		}
+	}
+
+	private static Atraccion vecinoMasPopular(Atraccion a, Graph<Atraccion, Vecindad> g, List<Atraccion> res) {
+		Set<Atraccion> vecinos = new HashSet<>();
+
+		// Itera sobre las vecindades de la atracción actual
+		for (Vecindad r : g.edgesOf(a)) {
+			Atraccion source = g.getEdgeSource(r);
+			Atraccion target = g.getEdgeTarget(r);
+
+			// Agrega vecinos que no han sido visitados a la lista
+			if (!res.contains(source)) {
+				vecinos.add(source);
+			}
+			if (!res.contains(target)) {
+				vecinos.add(target);
+			}
+		}
+
+		// Compara los vecinos por popularidad y devuelve el más popular
+		Comparator<Atraccion> comparacionVecinos = Comparator.comparing(Atraccion::popularidad);
+
+		return vecinos.stream().max(comparacionVecinos).orElse(null);
+	}
+
+	// Calcula el tiempo necesario para visitar una atracción con la información de la vecindad
+	private static double tiempoPorAtraccion(Atraccion a, Vecindad e) {
+		if (e != null) {
+			return (a.duracion() + a.tiempo() + e.tiempo()) / 60.0;
+		} else {
+			return 0.0;
+		}
 	}
 }
--- PracticaIndividual3/src/Tests/Test2.java
+++ PracticaIndividual3/src/Tests/Test2.java
@@ -1,5 +1,7 @@
 package Tests;
 
+import java.util.Comparator;
+import java.util.List;
 import java.util.Set;
 
 import org.jgrapht.Graph;
@@ -79,6 +81,8 @@ public class Test2 {
 		System.out.println("\nApartado B\n");
 		testEjercicio2B();
 		System.out.println("\n============================================================\n");
+		System.out.println("\nApartado C\n");
+		testEjercicio2C();
 	}
 
 	private static void testEjercicio2A() {
@@ -118,7 +122,7 @@ public class Test2 {
 				.findFirst().get();
 		Atraccion verticeDestino_3 = g3.vertexSet().stream().filter(v -> v.nombre().equals("Pim pam pum")).findFirst()
 				.get();
-		
+
 		System.out.println("Atraccion origen: " + verticeOrigen_3.nombre());
 		System.out.println("Atraccion destino: " + verticeDestino_3.nombre());
 		System.out.println("Camino mas corto en distancia: "
@@ -145,6 +149,71 @@ public class Test2 {
 				+ Ejercicios.Ejercicio2.ejercicio2B(g3_tiempo, fichero2_3).getVertexList().stream().map(v -> v.nombre())
 						.toList());
 
+	}
+
+	private static void testEjercicio2C() {
+
+		System.out.println("Fichero: " + fichero2_1);
+
+		Integer horas_1 = 5;
+
+		List<Atraccion> res_1 = Ejercicios.Ejercicio2.ejercicio2C(g1, horas_1);
+
+		System.out.println("Camino de atracciones mas populares con: " + horas_1 + " horas" + "\n"
+				+ res_1.stream().map(v -> v.nombre()).toList());
+
+		GraphColors.toDot(g1, carpetaResultados + fichero2_1 + "ApartadoC.gv", v -> v.nombre(),
+				e -> "(distancia: " + e.distanciaKm() + ", tiempo: " + e.tiempo() + ")",
+				v -> GraphColors.colorIf(List.of(res_1.get(0).equals(v), res_1.contains(v)),
+						List.of(Color.magenta, Color.blue)),
+				e -> GraphColors.colorIf(Color.blue, dibujarAristasC(g1, res_1, e)));
+
+		// ------------------------------------------------------------------------------------------------------------------------
+
+		System.out.println("\nFichero: " + fichero2_2);
+
+		Integer horas_2 = 2;
+
+		List<Atraccion> res_2 = Ejercicios.Ejercicio2.ejercicio2C(g2, horas_2);
+
+		System.out.println("Camino de atracciones mas populares con: " + horas_2 + " horas" + "\n"
+				+ res_2.stream().map(v -> v.nombre()).toList());
+
+		GraphColors.toDot(g2, carpetaResultados + fichero2_2 + "ApartadoC.gv", v -> v.nombre(),
+				e -> "(distancia: " + e.distanciaKm() + ", tiempo: " + e.tiempo() + ")",
+				v -> GraphColors.colorIf(List.of(res_2.get(0).equals(v), res_2.contains(v)),
+						List.of(Color.magenta, Color.blue)),
+				e -> GraphColors.colorIf(Color.blue, dibujarAristasC(g2, res_2, e)));
+
+		// ------------------------------------------------------------------------------------------------------------------------
+
+		System.out.println("\nFichero: " + fichero2_3);
+
+		Integer horas_3 = 3;
+
+		List<Atraccion> res_3 = Ejercicios.Ejercicio2.ejercicio2C(g3, horas_3);
+
+		System.out.println("Camino de atracciones mas populares con: " + horas_3 + " horas" + "\n"
+				+ res_3.stream().map(v -> v.nombre()).toList());
+
+		GraphColors.toDot(g3, carpetaResultados + fichero2_3 + "ApartadoC.gv", v -> v.nombre(),
+				e -> "(distancia: " + e.distanciaKm() + ", tiempo: " + e.tiempo() + ")",
+				v -> GraphColors.colorIf(List.of(res_3.get(0).equals(v), res_3.contains(v)),
+						List.of(Color.magenta, Color.blue)),
+				e -> GraphColors.colorIf(Color.blue, dibujarAristasC(g3, res_3, e)));
+
+	}
+
+	private static Boolean dibujarAristasC(Graph<Atraccion, Vecindad> g, List<Atraccion> ls, Vecindad edge) {
+
+		Boolean res = false;
+		for (int i = 0; i < ls.size() - 1; i++) {
+			if (g.getEdge(ls.get(i), ls.get(i + 1)).equals(edge)) {
+				res = true;
+				break;
+			}
+		}
+		return res;
 	}
 
 }
